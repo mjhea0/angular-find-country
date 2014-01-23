1e026cb466473b354edc1884525e570a90e75ad2
Michael Herman <hermanmu@gmail.com>
2014-01-22 22:26:00 -0700

    initial

---
+++ app.js
@@ -0,0 +1,52 @@
+var App = angular.module('angularCountry', []);
+
+function myController($scope, $http){
+  $scope.getInfo = function() {
+    initialize();
+    var address = 'http://restcountries.eu/rest/name/'+$scope.inputCountry;
+    $http({method: 'GET', url: address}).
+    success(function(data, status, headers, config) {
+      $scope.country = data[0];
+      findCountry(data[0]);
+    });
+    $scope.$watch('inputCountry', function() { console.log('watch!'); });
+  };
+};
+
+var map;
+geocoder = new google.maps.Geocoder();
+
+function initialize() {
+  var latLng = new google.maps.LatLng(48.825183,2.1975769);
+  var myOptions = {
+    panControl: true,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    overviewMapControl: false,
+    draggable: false,
+    disableDoubleClickZoom: false,
+    scrollwheel: false,
+    zoom: 6,
+    center: latLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP 
+  };
+  map = new google.maps.Map(document.getElementById("canvas"), myOptions);
+};
+
+function findCountry(country){
+  geocoder.geocode( { 'address': country.name}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      console.log(country.name)
+      console.log(results[0]);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      console.log(status)
+    }
+  });
+};
